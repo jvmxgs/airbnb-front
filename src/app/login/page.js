fa7b91d9b9445022efbc5ea62f8bc58951ef677b
Victor <jvmxgs@gmail.com>
2023-09-12 22:08:41 -0600

    changed route login

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -18,7 +18,7 @@ const Login = () => {
     e.preventDefault()
 
     try {
-      const response = await fetch('http://localhost:3000/login', {
+      const response = await fetch('http://localhost:3000/api/login', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
